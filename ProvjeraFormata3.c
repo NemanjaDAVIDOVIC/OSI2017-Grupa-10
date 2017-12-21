c4b541ae54f12d1d1bc235a259fc158128c81b16
IVANA <IVANA@IVANA-PC>
2017-12-21 20:49:34 +0100

    Provjera formata racuna 3 (datog na moodle-u)

---
+++ ProvjeraFormata3.c
@@ -0,0 +1,67 @@
+#include "ProvjeraFormata3.h"
+#include <string.h>
+#pragma warning(disable: 4996)
+
+char* provjeraFormata3()
+{
+	FILE* file;
+	int br = 0;
+	if (file = fopen("UcitaniRacun.txt", "r"))
+	{
+		char str[36];
+		fgets(str, 35, file);
+		fgets(str, 13, file);
+		if (!strcmp(str, "Poslovnica: "))
+			br++;
+		fgets(str, 27, file);
+		fgets(str, 8, file);
+		if (!strcmp(str, "Kupac: "))
+			br++;
+		fgets(str, 27, file);
+		fgets(str, 8, file);
+		if (!strcmp(str, "Datum: "))
+			br++;
+		fgets(str, 35, file);
+		fgets(str, 35, file);
+		fgets(str, 35, file);
+		if (!strcmp(str, "\tMaloprodajni racun\n"))
+			br++;
+		fgets(str, 35, file);
+		if (!strcmp(str, "\n"))
+			br++;
+		fgets(str, 36, file);
+		if (!strcmp(str, "Proizvod  Kolicina  Cijena  Ukupno\n"))
+			br++;
+		fgets(str, 36, file);
+		if (!strcmp(str, "----------------------------------\n"))
+			br++;
+		do
+		{
+			fgets(str, 37, file);
+		} while (strcmp(str, "----------------------------------\n"));
+		br++;
+		fgets(str, 35, file);
+		fgets(str, 8, file);
+		if (!strcmp(str, "Ukupno:"))
+			br++;
+		fgets(str, 7, file);
+		fgets(str, 6, file);
+		if (!strcmp(str, "PDV: "))
+			br++;
+		fgets(str, 35, file);
+		do
+		{
+			fgets(str, 2, file);
+		} while (strcmp(str, "\n"));
+		br++;
+		fgets(str, 21, file);
+		if (!strcmp(str, "Ukupno za placanje: "))br++;
+		if (br == 12)
+			return CORRECT;
+		else
+			return ERROR;
+		fclose(file);
+	}
+	else
+		return NOT_OPENED;
+}
