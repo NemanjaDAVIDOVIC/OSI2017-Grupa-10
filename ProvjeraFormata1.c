e17c43303d6cf04d866a94db68984375f39ebb67
Jovan <Jovan@LEGION>
2017-12-23 12:04:09 +0100

    Testirano i u komentarima ostavljene smjernice/sugestije/prijedlozi za izmjene.

--- ProvjeraFormata1.c
+++ ProvjeraFormata1.c
@@ -6,6 +6,7 @@ char* provjeraFormata1()
 {
 	FILE* file;
 	int br = 0;
+	/* TESTER: I ovdje kao i u prvojeti formata 3 bi bilo pozeljno da se datoteka navodi kao argument komandne linije. */
 	if (file = fopen("UcitaniRacun.txt", "r"))
 	{
 		char str[42];
@@ -17,13 +18,13 @@ char* provjeraFormata1()
 		if (!strcmp(str, "Datum: "))
 			br++;
 		fgets(str, 32, file);
-		fgets(str, 40, file);
+		fgets(str, 2, file); // TESTER: Dovoljno je da kupi 2 jer ima smao novi red - \n
 		if (!strcmp(str, "\n"))
 			br++;
 		fgets(str, 40, file);
 		if (!strcmp(str, "\t       Racun\n"))
 			br++;
-		fgets(str, 40, file);
+		fgets(str, 2, file); // TESTER: I ovdje je dovoljno da kupi 2
 		if (!strcmp(str, "\n"))
 			br++;
 		fgets(str, 40, file);
@@ -41,24 +42,24 @@ char* provjeraFormata1()
 			fgets(str, 2, file);
 		}
 		while (strcmp(str, "\n"));
-			br++;
-			fgets(str, 6, file);
-			if (!strcmp(str, "PDV: "))br++;
-			do
-			{
-				fgets(str, 2, file);
-			}
-			while (strcmp(str, "\n"));
-			br++;
-			fgets(str, 21, file);
-			if (!strcmp(str, "Ukupno za placanje: "))br++;
-			if (br == 12)
-				return CORRECT;
-			else
-				return ERROR;
-			fclose(file);
+		br++;
+		fgets(str, 6, file);
+		if (!strcmp(str, "PDV: ")) br++;
+		do
+		{
+			fgets(str, 2, file);
 		}
+		while (strcmp(str, "\n"));
+		br++;
+		fgets(str, 21, file);
+		if (!strcmp(str, "Ukupno za placanje: ")) br++;
+		if (br == 12)
+			return CORRECT;
+		else
+			return ERROR;
+		fclose(file);
+	}
+
 	else
 		return NOT_OPENED;
-		
-	}
+}
--- ProvjeraFormata1.h
+++ ProvjeraFormata1.h
@@ -1,6 +1,8 @@
 #ifndef PROVJERAFORMATA1_H
 #define PROVJERAFORMATA1_H
+
 #include  <stdio.h>
+
 #define CORRECT "Ucitani racun je ispravan."
 #define ERROR "Ucitani racun nije ispravan."
 #define NOT_OPENED "Ne moze se otvoriti datoteka za citanje."
