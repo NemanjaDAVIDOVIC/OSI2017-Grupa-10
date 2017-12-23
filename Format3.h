5c356b1aba219d18b6f97680e9dabe4d0705ef85
IVANA <IVANA@IVANA-PC>
2017-12-24 00:37:48 +0100

    Modifikacija koda za provjeru formata racuna 3

---
+++ Format3.h
@@ -0,0 +1,10 @@
+#ifndef PROVJERAFORMATA3_H
+#define PROVJERAFORMATA3_H
+#include  <stdio.h>
+#define CORRECT "Ucitani racun je ispravan."
+#define ERROR "Ucitani racun nije ispravan."
+#define NOT_OPENED "Ne moze se otvoriti datoteka za citanje."
+
+char* provjeraFormata3(char*);
+
+#endif
