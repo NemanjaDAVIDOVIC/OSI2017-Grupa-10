47c54dc1dca738e53a3b934bd58ac262535cd04d
Jovo Savic <jovo.savic@yahoo.com>
2018-01-18 11:31:13 +0100

    Manje izmjene u f.ji scandirr()

--- Scandir.c
+++ Scandir.c
@@ -21,5 +21,11 @@ char** scandirr()
 		return NULL;
 	}
 	files[i] = '\0'; //Stavlja (null) na kraj niza, sto je i granica niza;
-	return (files+2); //Sta je kod bez memory leak-a
+	for (int i = 0; files[i] != '\0'; i++)
+	{
+		char buffer[100];
+		getPath(buffer, files[i], racuni);
+		strcpy(files[i], buffer);
+	}
+	return (files + 2); //Sta je kod bez memory leak-a
 }
